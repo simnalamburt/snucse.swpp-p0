4e1bbdce505148d41d00f6a6038e71d0b58f2a0c
Hyeon Kim <simnalamburt@gmail.com>
2015-03-30 20:38:23 +0900

    Show

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,8 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find params[:id]
+  end
+
   def create
     @article = Article.new article_params
     @article.save
