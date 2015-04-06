6576345d48eb8bb14a342b5327cd8be8a75145f5
Hyeon Kim <simnalamburt@gmail.com>
2015-04-06 19:48:23 +0900

    글 수정

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -13,10 +13,25 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new article_params
+
     if @article.save
       redirect_to @article
     else
       render 'new'
+    end
+  end
+
+  def edit
+    @article = Article.find params[:id]
+  end
+
+  def update
+    @article = Article.find params[:id]
+
+    if @article.save
+      redirect_to @article
+    else
+      render 'edit'
     end
   end
 
